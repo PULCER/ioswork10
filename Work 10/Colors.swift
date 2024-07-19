29ce416e6bfb4bb54b8bf8379d161d458d58d3e7
PULCER <anthony@pulcer.net>
2024-07-19 16:06:40 -0700

    colors

---
+++ Work 10/Colors.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+extension Color {
+    static let customPink = Color(hex: 0xFFAEBC)
+    static let customTeal = Color(hex: 0xA0E7E5)
+    static let customGreen = Color(hex: 0xB4F8C8)
+    static let customYellow = Color(hex: 0xFBE7C6)
+}
+
+extension Color {
+    init(hex: UInt, alpha: Double = 1) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 08) & 0xff) / 255,
+            blue: Double((hex >> 00) & 0xff) / 255,
+            opacity: alpha
+        )
+    }
+}
