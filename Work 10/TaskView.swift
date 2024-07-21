94a1a2d4dd77505f7a75d20963d4469734eac8bb
Anthony Howell <anthony@pulcer.net>
2024-07-21 15:28:53 -0700

    legend

--- Work 10/TaskView.swift
+++ Work 10/TaskView.swift
@@ -6,11 +6,24 @@ struct TasksView: View {
     @Query private var items: [Item]
     @ObservedObject var navigationViewModel: NavigationViewModel
     
-    var allTasksWithItems: [(String, Item)] {
-        items.flatMap { item in
+    var sortedTasksWithItems: [(String, Item)] {
+        let allTasks = items.flatMap { item in
             (item.tasks ?? []).map { task in
                 (task, item)
             }
+        }
+        
+        return allTasks.sorted { (task1, task2) in
+            let components1 = task1.0.components(separatedBy: CharacterSet.decimalDigits.inverted)
+            let components2 = task2.0.components(separatedBy: CharacterSet.decimalDigits.inverted)
+            
+            if let number1 = Int(components1.first ?? ""), let number2 = Int(components2.first ?? "") {
+                if number1 != number2 {
+                    return number1 < number2
+                }
+            }
+            
+            return task1.0.localizedStandardCompare(task2.0) == .orderedAscending
         }
     }
     
@@ -22,7 +35,7 @@ struct TasksView: View {
             
             ScrollView {
                 LazyVStack(spacing: 10) {
-                    ForEach(allTasksWithItems, id: \.0) { task, item in
+                    ForEach(sortedTasksWithItems, id: \.0) { task, item in
                         Button(action: {
                             navigationViewModel.navigate(to: AnyView(AddItemView(modelContext: modelContext, editingItem: item, navigationViewModel: navigationViewModel)))
                         }) {
